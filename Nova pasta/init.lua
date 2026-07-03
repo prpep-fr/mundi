e4af9a0e0fcf69dc79dbfbb541f49dbbc390fa8b
prpep-fr <prpep.fr@gmail.com>
2026-07-03 03:37:04 -0300

    Add files via upload

--- Nova pasta/init.lua
+++ Nova pasta/init.lua
@@ -1,4 +1,6 @@
-local Window = require(script.Components.Window)
+local Window = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/prpep-fr/mundi/main/Nova%20pasta/Library/Components/Window.lua"
+))()
 
 local Library = {}
 Library.__index = Library
